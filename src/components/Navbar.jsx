2f8245082284b03ea7d1a1056c8b02573f73b2e5
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-18 14:21:19 +0330

    nav items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { appleImg } from '../utils'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <img src={appleImg} alt="apple logo" width={14} height={18} />
+
+            <div>
+                {['phones','Macbooks','Tablets'].map((nav,index)=>(
+                    <div key={index}>
+                        {nav}
+                    </div>
+                ))}
+            </div>
+        </nav>
+    </header>
   )
 }
