76bad96b06b7b5902082162277712b0703115595
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-19 10:21:01 +0330

    end nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { appleImg, bagImg, searchImg } from '../utils'
-import { navLists } from '../constants'
+import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-        <nav className='flex w-full screen-max-width'>
-            <img src={appleImg} alt="apple logo" width={14} height={18} />
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
 
-            <div className='flex flex-1 justify-center max-sm:hidden'>
-                {navLists.map((nav,index)=>(
-                    <div key={index} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
-                        {nav}
-                    </div>
-                ))}
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+              {nav}
             </div>
+          ))}
+        </div>
 
-            <div>
-                <img src={searchImg} alt="searchImg" width={18} height={18} />
-                <img src={bagImg} alt="bagImg" width={18} height={18} />
-            </div>
-        </nav>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
     </header>
   )
 }
+
+export default Navbar
