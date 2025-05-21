792f1ffb8dcd8c3e2dd5498a53c1fe2a7fe5b248
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-21 12:21:32 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-x9",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
