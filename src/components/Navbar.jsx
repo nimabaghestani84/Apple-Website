a186e2f799bdf73ad707cee6964372cf5458371f
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-22 15:55:53 +0330

    navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,12 @@ const Navbar = () => {
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
+        
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
       </nav>
     </header>
   )
