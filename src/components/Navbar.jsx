d8d82638b47fbcc66321a6f148be01c956aa62c0
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-22 15:54:39 +0330

    header style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      
+    </header>
   )
 }
 
