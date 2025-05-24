3847b6ad2da0a0ba9be78865dd78b2d15b2a9e36
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-24 23:03:31 +0330

    function of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,18 @@ import { heroVideo, smallHeroVideo } from "../utils";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 2 });
     gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
@@ -32,7 +44,9 @@ const Hero = () => {
         id="cta"
         className="flex flex-col items-center opacity-0 translate-y-20"
       >
-        <a href="#highlights" className="btn">Buy</a>
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
         <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
