5ac0f5f0da4d7041bca9e8c395b6de233ffc93d1
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-24 23:00:58 +0330

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
-import gsap from 'gsap';
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-  
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
-        
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
