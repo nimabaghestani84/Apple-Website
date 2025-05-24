aac580f79db657ef0d0a635a662234402fb6c0f1
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-24 23:02:41 +0330

    video for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,10 @@ import { heroVideo, smallHeroVideo } from "../utils";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
-  
   useGSAP(() => {
-    gsap.to('#hero', { opacity: 1, delay: 2 })
-    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
-  }, [])
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
 
   return (
     <section className="w-full nav-height bg-black relative">
@@ -16,6 +15,18 @@ const Hero = () => {
         <p id="hero" className="hero-title">
           iPhone 15 Pro
         </p>
+      </div>
+
+      <div className="md:w-10/12 w-9/12">
+        <video
+          className="pointer-events-none"
+          autoPlay
+          muted
+          playsInline={true}
+          key={videoSrc}
+        >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
       </div>
     </section>
   );
