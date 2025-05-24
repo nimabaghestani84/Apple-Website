4892a938a6fd59521844f2b590a3a2dbbce9e04f
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-24 23:01:46 +0330

    use gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,12 @@ import { heroVideo, smallHeroVideo } from "../utils";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
+  
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
+  }, [])
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
