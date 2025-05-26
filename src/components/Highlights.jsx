ae62e3169cb5a8d9001c01708201335b1e994637
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-26 13:06:06 +0330

    image of section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -18,7 +18,14 @@ const Highlights = () => {
           <h1 id="title" className="section-heading">Get the highlights.</h1>
 
           <div className="flex flex-wrap items-end gap-5">
-            
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
           </div>
         </div>
 
