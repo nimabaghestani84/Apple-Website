68de6f278b6bb6a4f4b1b08606048426a8d34684
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-28 11:32:11 +0330

    create base

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
 
-export default function VideoCarousel() {
+
+const VideoCarousel = () => {
+
+
   return (
-    <div>VideoCarousel</div>
-  )
-}
+    <>
+      <div className="flex items-center">
+     
+      </div>
+
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+         
+        </div>
+
+        
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
