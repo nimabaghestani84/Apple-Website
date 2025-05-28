b06506647c1cb58b04fb7d8635f73dbe91b96653
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-28 11:38:09 +0330

    div for button

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -53,12 +53,12 @@ const VideoCarousel = () => {
       </div>
 
       <div className="relative flex-center mt-10">
-        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
-         
-        </div>
-
-        
-      </div>
+              <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+                
+              </div>
+      
+             
+            </div>
     </>
   );
 };
