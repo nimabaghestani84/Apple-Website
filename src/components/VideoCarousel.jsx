572ae6b29cbe409e90e63caa3f21eaba59170b0c
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-28 11:34:31 +0330

    video player

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -16,7 +16,30 @@ const VideoCarousel = () => {
      {hightlightsSlides.map((list, i) => (
                <div key={list.id} id="slider" className="sm:pr-20 pr-10">
                  <div className="video-carousel_container">
-                   
+                   <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                     <video
+                       id="video"
+                       playsInline={true}
+                       className={`${
+                         list.id === 2 && "translate-x-44"
+                       } pointer-events-none`}
+                       preload="auto"
+                       muted
+                       ref={(el) => (videoRef.current[i] = el)}
+                       onEnded={() =>
+                         i !== 3
+                           ? handleProcess("video-end", i)
+                           : handleProcess("video-last")
+                       }
+                       onPlay={() =>
+                         setVideo((pre) => ({ ...pre, isPlaying: true }))
+                       }
+                       onLoadedMetadata={(e) => handleLoadedMetaData(i, e)}
+                     >
+                       <source src={list.video} type="video/mp4" />
+                     </video>
+                   </div>
+     
                  </div>
                </div>
              ))}
