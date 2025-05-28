895575a1e6b577d2d0ab0d8857080b8fe0f0548e
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-28 11:32:23 +0330

    imports

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,4 +1,11 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+import { useEffect, useRef, useState } from "react";
 
+import { hightlightsSlides } from "../constants";
+import { pauseImg, playImg, replayImg } from "../utils";
 
 const VideoCarousel = () => {
 
