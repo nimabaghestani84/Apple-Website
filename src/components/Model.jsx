418627f9cb567bcc2e6f1d5b7e7fa4064582dfe0
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-02 11:29:32 +0330

    use gsap

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,11 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
 
 const Model = () => {
+  useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
   return (
     <section className="common-padding">
       <div className="screen-max-width">
