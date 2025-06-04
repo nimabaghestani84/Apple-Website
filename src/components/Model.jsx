1cd6420dd7029abe83e2a6ff0fe46cc87dd67dac
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-04 14:12:44 +0330

    group for three js

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,15 +4,18 @@ import React, { useRef, useState } from "react";
 import ModelView from "./ModelView";
 
 const Model = () => {
-  const [size, setSize] = useState('small');
-    const [model, setModel] = useState({
-      title: 'iPhone 15 Pro in Natural Titanium',
-      color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
-      img: yellowImg,
-    })
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
 
-    const cameraControlSmall = useRef();
-      const cameraControlLarge = useRef();
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
 
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
@@ -26,7 +29,7 @@ const Model = () => {
 
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <ModelView/>
+            <ModelView />
           </div>
         </div>
       </div>
