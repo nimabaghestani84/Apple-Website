b0fe90990572ad39fc8c818ffd2d7cce7fe43433
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-09 22:54:04 +0330

    create base sections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,15 @@ import { footerLinks } from '../constants'
 const Footer = () => {
   return (
     <footer className="py-5 sm:px-10 px-5">
-      
+      <div className="screen-max-width">
+        <div>
+        </div>
+
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+        </div>
+      </div>
     </footer>
   )
 }
