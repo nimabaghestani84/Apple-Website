2ebb72ef453bccbe68c69d386efcf691d920eed8
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-09 22:55:08 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,15 @@ const Footer = () => {
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
-            
+            More ways to shop: {' '}
+            <span className="underline text-blue">
+            Find an Apple Store {' '}
+            </span>
+            or {' '}
+            <span className="underline text-blue">
+            other retailer
+            </span>{' '}
+            near you.
           </p>
           <p className="font-semibold text-gray text-xs">
             Or call 000800-040-1966
