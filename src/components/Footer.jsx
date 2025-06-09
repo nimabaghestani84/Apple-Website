15720485ec879cf296e04517d2d9c0136b6d9d2d
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-09 22:55:28 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,12 @@ const Footer = () => {
         <div className="flex md:flex-row flex-col md:items-center justify-between">
           <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
           <div className="flex">
-            
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{' '}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
               </p>
             ))}
           </div>
