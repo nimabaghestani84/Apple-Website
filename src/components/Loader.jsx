8c3f469aac3b4126090a1b9bec05f9bb7720a453
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-12 15:05:09 +0330

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 const Loader = () => {
   return (
     <Html>
-      
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        
+      </div>
     </Html>
   )
 }
