753d63e675628ab58b3f045d490447960826bdba
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-30 11:39:40 +0330

    create base

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,7 +8,13 @@ import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      
+    </View>
   )
 }
 
