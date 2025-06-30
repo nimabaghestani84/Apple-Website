d076f53231a6f708a78367afbd78b716045f358e
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-30 11:38:30 +0330

    imports

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import * as THREE from 'three'
+import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
+import { Suspense } from "react";
 
 const ModelView = () => {
   return (
