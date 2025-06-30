3671fe5e7338deddc4d54cc4494f982ef83662db
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-30 11:45:44 +0330

    animations

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -6,6 +6,27 @@ import gsap from 'gsap';
 import { animateWithGsap } from '../utils/animations';
 
 export default function HowItWorks() {
+   const videoRef = useRef();
+  
+    useGSAP(() => {
+      gsap.from('#chip', {
+        scrollTrigger: {
+          trigger: '#chip',
+          start: '20% bottom'
+        },
+        opacity: 0,
+        scale: 2,
+        duration: 2,
+        ease: 'power2.inOut'
+      })
+  
+      animateWithGsap('.g_fadeIn', {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.inOut'
+      })
+    }, []);
   return (
     <div>HowItWorks</div>
   )
