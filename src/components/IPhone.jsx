51b5cfad63e68f018216a5fa8e909905efd59abb
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-06-30 11:43:44 +0330

    Create IPhone.jsx

---
+++ src/components/IPhone.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as THREE from 'three';
+import React, { useEffect, useRef } from "react";
+import { useGLTF, useTexture } from "@react-three/drei";
+
+export default function IPhone() {
+  return (
+    <div>IPhone</div>
+  )
+}
