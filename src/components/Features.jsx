cd78a5facb92a4c67379aa63e65c7e1117ea54ef
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-07-03 12:05:59 +0330

    create base of components

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { useGSAP } from '@gsap/react'
-import React, { useRef } from 'react'
-import { animateWithGsap } from '../utils/animations';
-import { explore1Img, explore2Img, exploreVideo } from '../utils';
-import gsap from 'gsap';
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
+import { animateWithGsap } from "../utils/animations";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import gsap from "gsap";
 
 export default function Features() {
   return (
-    <div>Features</div>
-  )
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-wdith">
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 }
