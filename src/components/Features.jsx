9a1c47955d6ae201bb2b579a3fd117ed7cc8972b
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-07-03 12:07:55 +0330

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -36,6 +36,40 @@ export default function Features() {
               </video>
             </div>
           </div>
+          <div className="flex flex-col w-full relative">
+                        <div className="feature-video-container">
+                          <div className="overflow-hidden flex-1 h-[50vh]">
+                            <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
+                          </div>
+                          <div className="overflow-hidden flex-1 h-[50vh]">
+                            <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
+                          </div>
+                        </div>
+          
+                        <div className="feature-text-container">
+                          <div className="flex-1 flex-center">
+                            <p className="feature-text g_text">
+                              iPhone 15 Pro is {' '}
+                              <span className="text-white">
+                                the first iPhone to feature an aerospace-grade titanium design
+                              </span>,
+                              using the same alloy that spacecrafts use for missions to Mars.
+                            </p>
+                          </div>
+          
+                          <div className="flex-1 flex-center">
+                            <p className="feature-text g_text">
+                              Titanium has one of the best strength-to-weight ratios of any metal, making these our {' '}
+                              <span className="text-white">
+                                lightest Pro models ever.
+                              </span>
+                              You'll notice the difference the moment you pick one up.
+                            </p>
+                          </div>
+          
+          
+                        </div>
+                      </div>
         </div>
       </div>
     </section>
