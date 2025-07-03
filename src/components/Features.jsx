64876e1d3d82764da5af3f53d34e9f158aa73fa2
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-07-03 12:07:19 +0330

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -21,6 +21,21 @@ export default function Features() {
               Forged in titanium.
             </h2>
           </div>
+          <div className="flex-center flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video
+                playsInline
+                id="exploreVideo"
+                className="w-full h-full object-cover object-center"
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
