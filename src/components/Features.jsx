2028f0abdda04a7b37fc956456347f443a240f37
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-07-03 12:05:05 +0330

    Create Features.jsx

---
+++ src/components/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useGSAP } from '@gsap/react'
+import React, { useRef } from 'react'
+import { animateWithGsap } from '../utils/animations';
+import { explore1Img, explore2Img, exploreVideo } from '../utils';
+import gsap from 'gsap';
+
+export default function Features() {
+  return (
+    <div>Features</div>
+  )
+}
